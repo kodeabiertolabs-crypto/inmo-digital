9e98f923e01c8ccb2384cff0bde61162814471f5
javiarch <javidiaz1977@gmail.com>
2025-10-09 16:16:14 -0300

    Cambio en vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,11 @@ import preact from '@preact/preset-vite'
 import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
+const repoName = 'inmo-digital'; 
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [preact()],
-  base: '/inmo-digital/',
-  build: {
-    outDir: 'dist' // usa 'dist' (por defecto de Vite)
-  } 
+  base: `/${repoName}/`,
+   
 })
